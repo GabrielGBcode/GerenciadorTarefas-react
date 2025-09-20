0d325ded6e3eae20a8500b796fb8105c632cd7f7
gabrielgbcode <gabrielgb.xyz@gmail.com>
2025-09-19 21:17:29 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import Tasks from "./components/tasks";
+import AddTask from "./components/AddTask";
+import Title from "./components/Title";
+import Test from "./components/Test";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      // Chamando a API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "POST",
+        }
+      );
+
+      // Pegar os dados da API
+      const data = await response.json();
+
+      // Armazenar os dados no estado
+      setTasks(data);
+    }
+
+    // fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      {/* <Test /> */}
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+export default App;
