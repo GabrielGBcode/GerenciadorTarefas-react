62c2cb4cf26178e5186b0526e1c2be65eec49fae
gabrielgbcode <gabrielgb.xyz@gmail.com>
2025-09-19 21:44:25 -0300

    adding icon for completed tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,10 +18,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-white text-left w-full p-2 rounded-md ${
+            className={`bg-slate-400 text-white flex items-center gap-2 text-left w-full p-2 rounded-md ${
               task.completed ? "line-through" : ""
             }`}
           >
+            {task.completed ? <CheckIcon /> : " "}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
